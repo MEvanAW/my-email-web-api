9cce8ffa2e6223cfd1d572f2091731c916b535e5
M. Evan Anindya W <m.evan.aw@gmail.com>
2025-10-15 15:46:41 +0700

    Send email using MailKit SMTP Client.

--- MyEmailWebApi/Controllers/EmailController.cs
+++ MyEmailWebApi/Controllers/EmailController.cs
@@ -1,5 +1,7 @@
 using Ganss.Xss;
+using MailKit.Net.Smtp;
 using Microsoft.AspNetCore.Mvc;
+using MimeKit;
 using MyEmailWebApi.DTO;
 
 namespace MyEmailWebApi.Controllers
@@ -8,30 +10,65 @@ namespace MyEmailWebApi.Controllers
     [Route("[controller]")]
     public class EmailController : ControllerBase
     {
-        private const string BAD_BODY = "Missing attribute or body format is not recognized.";
+        #region const
         private const string CLASS = "class";
+        private const string FROM_ADDRESS = "FromAddress";
+        private const string FROM_NAME = "FromName";
+        private const string HOST = "Host";
+        private const string PASSWORD = "Password";
+        private const string PORT = "Port";
+        private const string RECIPIENT = "Recipient";
+        private const string SEND_EMAIL_RESPONSE_FORMAT = "Email has been sent to {0}.";
+        private const string SMTP = "Smtp";
         private const string STYLE = "style";
+        private const string USE_SSL = "UseSsl";
+        private const string USERNAME = "Username";
+        #endregion
+
+        private readonly IConfigurationSection _smtpConfig;
         private readonly ILogger<EmailController> _logger;
         // docs: https://www.nuget.org/packages/HtmlSanitizer/#readme-body-tab
-        private readonly HtmlSanitizer sanitizer;
+        private readonly HtmlSanitizer _sanitizer;
 
-        public EmailController(ILogger<EmailController> logger)
+        public EmailController(ILogger<EmailController> logger, IConfiguration configRoot)
         {
+            _smtpConfig = configRoot.GetSection(SMTP);
             _logger = logger;
-            sanitizer = new();
-            sanitizer.AllowedTags.Add(STYLE);
-            sanitizer.AllowedAttributes.Add(CLASS);
+            _sanitizer = new();
+            _sanitizer.AllowedTags.Add(STYLE);
+            _sanitizer.AllowedAttributes.Add(CLASS);
         }
 
         [HttpPost(Name = "SendEmail")]
         public IActionResult SendEmail([FromBody] SendEmailRequest request)
         {
+            // Building message.
+            var bodyBuilder = new BodyBuilder();
+            var message = new MimeMessage();
+            message.From.Add(new MailboxAddress(_smtpConfig[FROM_NAME] ?? string.Empty, _smtpConfig[FROM_ADDRESS]));
+            message.To.Add(new MailboxAddress(RECIPIENT, request.Recipient));
+            if (request.Subject is not null)
+            {
+                bodyBuilder.TextBody = request.Subject;
+                message.Subject = request.Subject;
+            }
             if (request.Body is not null)
             {
                 // Sanitize the body to prevent XSS attack or similar. Will strip script tag and onload attribute
-                request.Body = sanitizer.Sanitize(request.Body);
+                bodyBuilder.HtmlBody = _sanitizer.SanitizeDocument(request.Body);
             }
-            return Ok(request);
+            message.Body = bodyBuilder.ToMessageBody();
+
+            // Sending email.
+            // docs: https://github.com/jstedfast/MailKit?tab=readme-ov-file#sending-messages
+            using (var client = new SmtpClient())
+            {
+                client.Connect(_smtpConfig[HOST], _smtpConfig.GetValue<int>(PORT, 587), _smtpConfig.GetValue<bool>(USE_SSL, false));
+                client.Authenticate(_smtpConfig[USERNAME], _smtpConfig[PASSWORD]);
+                client.Send(message);
+                client.Disconnect(true);
+            }
+            return Ok(string.Format(SEND_EMAIL_RESPONSE_FORMAT, request.Recipient));
         }
     }
 }
